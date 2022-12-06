31d4cf55418f66240f26920488ece0e375eb1612
Yuri Reis <yurireis@discente.ufg.br>
2022-12-06 16:35:05 -0300

    Update Servidor.py

--- Questao7/Socket/Servidor.py
+++ Questao7/Socket/Servidor.py
@@ -30,16 +30,3 @@ while True:
 
         print('Finalizando conexão do cliente', cliente)
         conexao.close()
-
-        PORT = 8080
-
-        destino = (HOST, PORT)
-
-        tcp.connect(destino)
-
-        if int(mensagem.decode().split()[0])>=65 or int(mensagem.decode().split()[1])>=30 or int((mensagem.decode().split()[0])>=60 & int(mensagem.split()[1])>=25):
-                tcp.send(str("Pode se aposentar!").encode())
-        else: 
-            tcp.send(str("Não pode se aposentar!").encode())
-
-        tcp.close()
