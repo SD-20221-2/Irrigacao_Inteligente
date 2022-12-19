31e7dee4a998455d594a435000590925f27441c8
Yuri Reis <yurireis@discente.ufg.br>
2022-12-18 23:07:13 -0300

    Delete Entrega_da_Aula_13-12-2022 directory

--- Entrega_da_Aula_13-12-2022/Questao7/RMI/Cliente.py
+++
@@ -1,8 +0,0 @@
-import Pyro4
-aposentadoria = Pyro4.Proxy("PYRONAME:aposentadoria")
-
-idade = int(input("Qual a idade do funcionário? "))
-
-tempoServico = int(input("Qual o tempo de serviço do funcionário? "))
-
-print(aposentadoria.podeAposentar(idade,tempoServico))
