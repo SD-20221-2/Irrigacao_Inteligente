d70143fe6ec67935f5f21d481cebeb227d8e7ae3
Douglas Sousa Jorge <douglas.sousa541@hotmail.com>
2023-02-15 23:47:01 -0300

    testes da rota post

--- Projeto/Publisher/publisher.py
+++ Projeto/Publisher/publisher.py
@@ -2,21 +2,91 @@ import time
 import zmq
 
 base_url = "192.168.100.22"
-port = "2020"
-url = "tcp://" + base_url + ":" + port
+statusPort = "2020"
+paramsPort = "2021"
+urlStatus = f"tcp://{base_url}:{statusPort}"
+urlParams = f"tcp://{base_url}:{paramsPort}"
 
-def main():
+
+def send_status():
     context = zmq.Context()
     publisher = context.socket(zmq.PUB)
-    publisher.bind(url)
+    publisher.bind(urlStatus)
 
     while True:
-        publisher.send_multipart([b"umidade", b'{ "precisaRegar":true,"umidade":20}'])
+        publisher.send_multipart(
+            [b"status", b'{ "precisaRegar":true,"umidade":20}'])
         time.sleep(1)
 
     publisher.close()
     context.term()
 
 
+def main():
+    send_status()
+
+
 if __name__ == "__main__":
     main()
+
+# ------------------------------------------------------------------------
+
+# import json
+# import time
+# import zmq
+
+# base_url = "192.168.100.22"
+# statusPort = "2020"
+# paramsPort = "2021"
+# urlStatus = f"tcp://{base_url}:{statusPort}"
+# urlParams = f"tcp://{base_url}:{paramsPort}"
+
+
+# def receive_data():
+#     get_context = zmq.Context()
+#     subscriber = get_context.socket(zmq.SUB)
+#     subscriber.connect(urlParams)
+#     subscriber.setsockopt(zmq.SUBSCRIBE, b"umidade")
+
+#     poller = zmq.Poller()
+#     poller.register(subscriber, zmq.POLLIN)
+
+#     try:
+#         socks = dict(poller.poll(1000))
+#     except KeyboardInterrupt:
+#         subscriber.close()
+#         get_context.term()
+#         exit()
+
+#     if subscriber in socks:
+#         [address, contents] = subscriber.recv_multipart()
+#         conteudo = json.loads(contents)
+#         subscriber.close()
+#         get_context.term()
+#         print(conteudo)
+#         return conteudo
+
+
+# def send_data(data):
+#     send_context = zmq.Context()
+#     publisher = send_context.socket(zmq.PUB)
+#     publisher.bind(urlStatus)
+
+#     publisher.send_multipart([b"status", json.dumps(data).encode()])
+#     publisher.close()
+#     send_context.term()
+
+
+# def main():
+#     while True:
+#         # data = receive_data()
+#         # if data:
+#         #     # Aqui você pode fazer o processamento necessário com os dados recebidos
+#         #     print("Dados recebidos:", data)
+
+#         send_data({"precisaRegar": False, "umidade": 30})
+#         time.sleep(1)
+
+
+# if __name__ == "__main__":
+#     main()
--- Projeto/Subscriber/subscriber.py
+++ Projeto/Subscriber/subscriber.py
@@ -13,6 +13,8 @@
 # pela disponibização de endpoints para que seja realizada a integração
 # Telegram - Servidor.
 
+
+import time
 import zmq
 import json
 from fastapi import FastAPI
@@ -20,15 +22,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 base_url = "192.168.100.22"
-port = "2020"
-url = "tcp://" + base_url + ":" + port
+statusPort = "2020"
+paramsPort = "2021"
+urlStatus = f"tcp://{base_url}:{statusPort}"
+urlParams = f"tcp://{base_url}:{paramsPort}"
 
 
-def recuperar_dados():
+def receive_status():
     context = zmq.Context()
     subscriber = context.socket(zmq.SUB)
-    subscriber.connect(url)
-    subscriber.setsockopt(zmq.SUBSCRIBE, b"umidade")
+    subscriber.connect(urlStatus)
+    subscriber.setsockopt(zmq.SUBSCRIBE, b"status")
 
     poller = zmq.Poller()
     poller.register(subscriber, zmq.POLLIN)
@@ -48,6 +52,30 @@ def recuperar_dados():
         return conteudo
 
 
+def send_params():
+    send_context = zmq.Context()
+    publisher = send_context.socket(zmq.PUB)
+    publisher.bind(urlParams)
+
+    publisher.send_multipart([b"params", b'{ "cultura":"Alface"}'])
+    publisher.close()
+    send_context.term()
+
+
+def main():
+    while True:
+        print(receive_status())
+
+
 @app.get("/")
-def getRegarOuNao():
-    return recuperar_dados()
+def getData():
+    return receive_status()
+
+
+# @app.post("/")
+# def postData():
+#     return send_params()
+
+
+if __name__ == "__main__":
+    main()
